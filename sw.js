4a9963219cc928a7dad4ecfc4e3d007b7cd50807
Nolan-here <waxwz668@gmail.com>
2026-08-23 15:19:05 +0800

    Update sw.js for B1 group explanations

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'tcm-exam-v1-20260823-16';
+const CACHE_NAME = 'tcm-exam-v1-20260823-17';
 const APP_SHELL = [
   './', './index.html', './styles.css?v=9', './manifest.webmanifest', './assets/icon.svg',
-  './js/app.js?v=15', './js/db.js', './js/questions-bank.js',
+  './js/app.js?v=16', './js/db.js', './js/questions-bank.js',
   './js/questions-2024.js', './js/questions-2023.js', './js/questions-2018-2022.js',
   './js/questions-2018.js', './js/questions-2019.js', './js/questions-2020.js',
   './js/questions-2021.js', './js/questions-2022.js'
